1930e16de92ec97281c223a559d2bcc245a2c4f2
TempCSharp <36564655+TempCSharp@users.noreply.github.com>
2018-02-20 22:14:12 +0300

    Update files

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Console;
 
 namespace HelloWorld
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            WriteLine("Hello World!");
+            ReadKey();
         }
     }
 }
